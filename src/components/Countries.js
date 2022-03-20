55ed3fb09d0c04d07b2986754b7a265eefae2872
caugnon <joffreycaugnon@gmail.com>
2022-03-20 10:57:26 +0100

    continent selection

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -5,6 +5,8 @@ import Card from './Card';
 const Countries = () => {
     const [data, setData] = useState([])
     const [rangeValue, setRangeValue] = useState(36);
+    const [selectedRadio, setSelectedRadio] = useState("")
+    const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
     
     useEffect(() => {
         axios
@@ -22,10 +24,31 @@ const Countries = () => {
                     defaultValue={rangeValue}
                     onChange={(e) => setRangeValue(e.target.value)}
                 /> 
+                {radios.map((continent) => (
+                    <li>
+                        <input 
+                        type="radio" 
+                        id={continent} 
+                        name='continentRadio' 
+                        checked={continent === selectedRadio}
+                        onChange={(e) => setSelectedRadio(e.target.id)}
+                        />
+                        <label htmlFor={continent}>{continent}</label>
+                    </li>
+                ))}
             </ul>
+            {selectedRadio && (
+                <button onClick={() => setSelectedRadio('')}>
+                    Annuler la recherche
+                </button>
+            )}
             <ul>
-                {data.slice(0, rangeValue).map((country, index) =>(
-                    <Card key={index} country={country}/>
+                {data
+                    .filter((country) => country.continents[0].includes(selectedRadio))
+                    .sort((a, b) => b.population - a.population)
+                    .slice(0, rangeValue)
+                    .map((country, index) =>(
+                        <Card key={index} country={country}/>
                 ))
                 }
             </ul>
